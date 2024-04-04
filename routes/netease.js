af52ebec912e022369a8220ee158413d47637714
huangweiye <huangweiye@chengyun.biz>
2024-04-04 13:35:19 +0800

    调整

--- routes/netease.js
+++ routes/netease.js
@@ -25,11 +25,13 @@ const langMap = {
 
 const options = {
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
     'Content-Type': 'application/x-www-form-urlencoded',
     'Referer': 'https://music.163.com',
     'Cookie': '__remember_me=true; NMTID=xxx',
   },
+  httpAgent: new http.Agent({ keepAlive: true }),
+  httpsAgent: new https.Agent({ keepAlive: true }),
 }
 
 router.get('/', ctx => {
