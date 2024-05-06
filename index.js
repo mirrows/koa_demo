5aeeb1b06402edc8746adac528c1c49dd1159220
huangweiye <huangweiye@emodor.com>
2024-05-06 15:32:35 +0800

    yulanc

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ app.use(async (ctx, next) => {
   } else {
     // try {
       await next();   // 执行后代的代码
-      console.log(ctx.connection, ctx.get('Connection'))
+      console.log(555, ctx.connection)
+      console.log(5444, ctx.get('connection'))
       console.log(ctx)
-      if (!ctx.body && ctx.Connection !== 'keep-alive') {  // 没有资源
+      if (!ctx.body && ctx.connection !== 'keep-alive') {  // 没有资源
         ctx.status = 404;
         ctx.body = { code: 404, msg: 'page has loss' };
       }
