d2250238fb9c1528596fde5075f7342609b8871a
huangweiye <huangweiye@emodor.com>
2024-05-06 16:36:39 +0800

    34344

--- routes/gemini.js
+++ routes/gemini.js
@@ -30,25 +30,28 @@ async function displayChatTokenCount(model, chat, msg) {
 }
 
 router.post('/text', async (ctx) => {
+  const { msg } = ctx.request.body
+  ctx.request.socket.setTimeout(0);
+  ctx.req.socket.setNoDelay(true);
+  ctx.req.socket.setKeepAlive(true);
   ctx.set({
     'Content-Type': 'text/event-stream',
     'Cache-Control': 'no-cache',
     'Connection': 'keep-alive'
   });
   console.log('请求进入');
-  const { msg } = ctx.request.body
-  const chat = model.startChat({})
-  displayChatTokenCount(model, chat, msg);
-  const result1 = await chat.sendMessageStream(msg);
-  const stream = new PassThrough();
-  ctx.status = 200;
-  ctx.body = stream;
-  await streamToStdout(stream, result1.stream);
-  // 当客户端关闭连接时清除定时器
   ctx.req.on('close', () => {
     console.log('连接关闭')
     // ctx.body=""
   });
+  // const chat = model.startChat({})
+  // displayChatTokenCount(model, chat, msg);
+  const result1 = await chat.sendMessageStream(msg);
+  ctx.status = 200;
+  ctx.body = result1.stream;
+  // await streamToStdout(stream, result1.stream);
+  // 当客户端关闭连接时清除定时器
+  
 })
 
 
