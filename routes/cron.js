a45f619f569b93513ef23e606ea1db1ae3b91a36
huangweiye <huangweiye@emodor.com>
2024-05-14 17:45:04 +0800

    设置

--- routes/cron.js
+++ routes/cron.js
@@ -6,50 +6,60 @@ const { imgUrlToBase64 } = require('../utils/imgTool');
 const router = require('koa-router')(); //引入并实例化
 
 router.get('/bing', async ctx => {
-  const { status, data } = await req.get('https://bing.com/HPImageArchive.aspx', {
-    params: { format: 'js', n: 1 },
-  })
-  if (status !== 200) {
-    ctx.status = 500
+  const { time, url, name = 'bing.jpg' } = ctx.request.query
+  let todayBingUrl = url
+  if (!time) {
+    const { status, data } = await req.get('https://bing.com/HPImageArchive.aspx', {
+      params: { format: 'js', n: 1 },
+    })
+    if (status !== 200) {
+      ctx.status = 500
+      return ctx.body = {
+        code: 500,
+        msg: '请求失败'
+      }
+    }
+    todayBingUrl = `https://bing.com${data.images[0].url}`
+    // const binPicData = {
+    //   ...data.images[0],
+    //   url: `https://bing.com${data.images[0].url}`
+    // }
+  } else if (!url) {
     return ctx.body = {
-      code: 500,
-      msg: '请求失败'
+      code: 403,
+      msg: '请设置图片链接: url'
+    }
+  }
+  
+  const base64 = await imgUrlToBase64(todayBingUrl)
+  const mode = 'photo'
+  const realPath = `normal/${Format(time ? new Date(time) : new Date(), 'YYYY_MM_DD')}/${name}`
+  // https://wsrv.nl/?url=raw.githubusercontent.com/mirrows/photo/main/normal/2024_05_09/pic1715235436149104.png&w=300&fit=cover&n=-1&q=80
+  const res = await req.put(`https://api.github.com/repos/${gUser}/${mode}/contents/${realPath}`, {
+    content: base64.data,
+    message: `create ${realPath} img`
+  }, {
+    headers: {
+      Accept: "application/vnd.github+json",
+      Authorization: githubToken,
+    },
+  }).catch(err => {
+    console.log(err.response.data)
+    return err.response?.data || err
+  })
+  if (res?.data) {
+    const f = res.data.content
+    ctx.body = {
+      code: 0,
+      data: f,
     }
   } else {
-    const binPicData = {
-      ...data.images[0],
-      url: `https://bing.com${data.images[0].url}`
-    }
-    const base64 = await imgUrlToBase64(binPicData.url)
-    const mode = 'photo'
-    const realPath = `normal/${Format(new Date(), 'YYYY_MM_DD')}/bing.jpg`
-    // https://wsrv.nl/?url=raw.githubusercontent.com/mirrows/photo/main/normal/2024_05_09/pic1715235436149104.png&w=300&fit=cover&n=-1&q=80
-    const res = await req.put(`https://api.github.com/repos/${gUser}/${mode}/contents/${realPath}`, {
-      content: base64.data,
-      message: `create ${realPath} img`
-    }, {
-      headers: {
-        Accept: "application/vnd.github+json",
-        Authorization: githubToken,
-      },
-    }).catch(err => {
-      console.log(err.response.data)
-      return err.response?.data || err
-    })
-    if (res?.data) {
-      const f = res.data.content
-      ctx.body = {
-        code: 0,
-        data: f,
-      }
-    } else {
-      ctx.status = 403
-      // console.log(res)
-      ctx.body = {
-        code: 403,
-        msg: '请求出错，请联系管理员',
-        err: res,
-      }
+    ctx.status = 403
+    // console.log(res)
+    ctx.body = {
+      code: 403,
+      msg: '请求出错，请联系管理员',
+      err: res,
     }
   }
 })
