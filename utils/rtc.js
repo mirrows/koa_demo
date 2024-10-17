adc44ba33ee132b78884ca9ae3b2f9b5fa86e9b9
huangweiye <pmstalt@outlook.com>
2024-10-17 15:59:46 +0800

    调整

--- utils/rtc.js
+++ utils/rtc.js
@@ -4,9 +4,7 @@ const rooms = new Map()
 const socketInstance = {}
 
 function initRtc(server) {
-  const io = new Server(server, {
-    cors: true,
-  })
+  const io = new Server(server)
 
   io.on('connection', (socket) => {
     socket.emit('connected', socket.id)
