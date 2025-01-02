ccf552b52e6ae7f99435c73e650755f54bf6719f
huangweiye <pmstalt@outlook.com>
2025-01-02 17:04:37 +0800

    rtc设置完善

--- utils/rtc.js
+++ utils/rtc.js
@@ -2,28 +2,24 @@ const { Server } = require("socket.io")
 
 const rooms = new Map()
 const socketInstance = {}
+const GLOBAL_ROOM = 'common_room'
 
 function initRtc(server) {
   const io = new Server(server)
 
   io.on('connection', (socket) => {
+    console.log('socket connection started');
     socket.emit('connected', socket.id)
 
     socket.on('create_or_join_room', ({ info, roomId: targetRoomId }) => {
       const { roomId, socketId } = info
-      const curRoomUsers = rooms.get(targetRoomId || info.roomId) || []
-      console.log(
-        targetRoomId,
-        roomId,
-        curRoomUsers.length,
-        rooms.get(targetRoomId),
-        rooms.get(roomId)
-      )
+      const users = rooms.get(targetRoomId || info.roomId) || []
+      const curRoomUsers = users.filter(user => socketInstance[user.socketId])
+
       if (targetRoomId && curRoomUsers.length === 0) {
         socket.emit('room_joined', { user: info, another: info, result: 404 })
         return
       }
-      console.log(curRoomUsers)
       if(curRoomUsers.length >= 2) {
         socket.emit('room_full', roomId)
       } else {
@@ -46,10 +42,14 @@ function initRtc(server) {
     })
     socket.on('join', ({ info }) => {
       const { socketId } = info
-      const curRoomUsers = rooms.get("common_room") || []
-      socket.emit('join', {
+      socket.join(GLOBAL_ROOM)
+      const curRoomUsers = rooms.get(GLOBAL_ROOM) || []
+      const users = curRoomUsers.filter(user => socketInstance[user.socketId]).concat(info);
+      rooms.set(GLOBAL_ROOM, users)
+      socketInstance[socketId] = socket
+      io.to(GLOBAL_ROOM).emit('join', {
         user: info,
-        another: curRoomUsers.filter(user => user.socketId !== socketId),
+        another: users,
       })
     })
     
@@ -67,7 +67,7 @@ function initRtc(server) {
     })
 
     socket.on('add_candidate', ({info, candidate, to: other}) => {
-      if (!other) return;
+      if (!other || !socketInstance[other.socketId]) return;
       socketInstance[other.socketId].emit('add_candidate', {info, candidate, to: other})
     })
 
@@ -95,14 +95,14 @@ function initRtc(server) {
         socketInstance[info.socketId].leave(info.roomId);
         delete socketInstance[info.socketId]
       }
-      const curRoomUsers = rooms.get('common_room') || []
+      const curRoomUsers = rooms.get(GLOBAL_ROOM) || []
       if (!curRoomUsers.some(item => item.socketId === info.socketId)) return
       const other = curRoomUsers.filter(item => item.socketId !== info.socketId)
       console.log('room leave', curRoomUsers, info)
       if (other && other.length) {
         rooms.set(info.roomId, [...other])
         other.forEach(user => {
-          socketInstance[user.socketId].emit('leave', info)
+          socketInstance[user.socketId]?.emit('leave', info)
         })
         // socketInstance[other.socketId].emit('room_leave', info)
       } else {
@@ -112,14 +112,16 @@ function initRtc(server) {
 
     // 监听 disconnect 事件
     socket.on('disconnect', (reason) => {
-      console.log('User disconnected', reason);
+      console.log('User disconnected', reason, socket.id);
       // 这里可以执行清理工作，比如移除用户相关的状态信息
+      delete socketInstance[socket.id];
     });
 
     // 监听 disconnect 事件
     socket.on('close', (reason) => {
       console.log('connection closed', reason);
       // 这里可以执行清理工作，比如移除用户相关的状态信息
+      delete socketInstance[socket.id];
     });
   })
 }
